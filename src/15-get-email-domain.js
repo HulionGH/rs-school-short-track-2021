d75359e06158f76fa657be4bc717e505170785ea
HulionGH <julia.gramovich@mail.ru>
2021-04-11 22:13:54 +0300

    feat: add find-index and get-domain func

--- src/15-get-email-domain.js
+++ src/15-get-email-domain.js
@@ -8,8 +8,10 @@
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new Error('Not implemented');
+function getEmailDomain(email) {
+  const array = email.split('');
+  const index = array.lastIndexOf('@');
+  return email.slice(index + 1);
 }
 
 module.exports = getEmailDomain;
